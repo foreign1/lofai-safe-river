639570ce2f0e654a20bd494219a71020f97b45e5
foreign1 <jenyojohn24@gmail.com>
2023-09-25 20:30:50 +0100

    update script to remove node install in project root

--- setup.bash
+++ setup.bash
@@ -1,16 +1,8 @@
 #!/bin/bash
 
 git clone git@github.com:foreign1/lofai-FE.git
-cd frontend
-npm install
-
-cd ..
 
 git clone git@github.com:foreign1/lofai-BE.git
-cd backend
-npm install
-
-cd ..
 
 mkdir data
 
